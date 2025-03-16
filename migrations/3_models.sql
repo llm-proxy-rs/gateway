e567229243ccbc476b334101b226e0dace13a406
whs <hswongac@gmail.com>
2025-03-15 23:00:23 -0400

    Fix model name and add models seed

--- migrations/3_models.sql
+++ migrations/3_models.sql
@@ -2,7 +2,7 @@ create table if not exists models (
     created_at timestamptz not null default now(),
     input_price_per_token numeric(10, 10),
     model_id uuid primary key default uuid_generate_v4(),
-    model_name varchar(255) not null,
+    model_name varchar(255) not null unique,
     output_price_per_token numeric(10, 10),
     updated_at timestamptz not null default now()
 );
