f633e9d6c560b1f1970754e6fb0cf1af7e2bb5cb
whs <hswongac@gmail.com>
2025-03-21 22:32:00 -0400

    Test

--- default.nix
+++ default.nix
@@ -8,18 +8,36 @@
     useFetchCargoVendor = true;
     version = "0.1.0";
   };
+  entrypointScript = pkgs.writeScriptBin "entrypoint.sh" ''
+    #!/bin/sh
+    set -e
+
+    echo "Running database migrations..."
+    sqlx migrate run
+
+    echo "Starting gateway server..."
+    server
+  '';
+  migrations = pkgs.runCommand "migrations" {} ''
+    mkdir -p $out/migrations
+    cp -r ${./migrations}/* $out/migrations/
+  '';
 in
   pkgs.dockerTools.buildImage {
     name = "gateway";
     tag = "latest";
     config = {
-      Cmd = ["server"];
+      Entrypoint = ["${entrypointScript}/bin/entrypoint.sh"];
     };
     copyToRoot = pkgs.buildEnv {
       name = "image-root";
       paths = [
+        entrypointScript
+        migrations
+        pkgs.bash
         pkgs.dockerTools.caCertificates
         rustPackage
+        sqlx-cli
       ];
     };
   }
