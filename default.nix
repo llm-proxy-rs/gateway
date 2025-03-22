01b736c0b7f191af4c987a12ea79869b7c741a47
whs <hswongac@gmail.com>
2025-03-21 22:32:00 -0400

    Add migrations

--- default.nix
+++ default.nix
@@ -8,18 +8,33 @@
     useFetchCargoVendor = true;
     version = "0.1.0";
   };
+  entrypointScript = pkgs.writeScriptBin "entrypoint.sh" ''
+    #!${pkgs.bash}/bin/bash
+    set -e
+
+    echo "Running database migrations..."
+    ${pkgs.sqlx-cli}/bin/sqlx migrate run
+
+    echo "Starting gateway server..."
+    exec ${rustPackage}/bin/server
+  '';
+  migrations = pkgs.runCommand "migrations" {} ''
+    mkdir -p $out/migrations
+    cp -r ${./migrations}/* $out/migrations/
+  '';
 in
   pkgs.dockerTools.buildImage {
     name = "gateway";
     tag = "latest";
     config = {
-      Cmd = ["server"];
+      Entrypoint = ["${entrypointScript}/bin/entrypoint.sh"];
     };
     copyToRoot = pkgs.buildEnv {
       name = "image-root";
       paths = [
+        entrypointScript
+        migrations
         pkgs.dockerTools.caCertificates
-        rustPackage
       ];
     };
   }
