8a486609bd8c5a180444b42e5f1b9bb2d27c58cd
whs <hswongac@gmail.com>
2026-01-29 09:50:39 +0000

    Test

--- migrations/5_models_seed.sql
+++ migrations/5_models_seed.sql
@@ -1,2 +1,4 @@
 INSERT INTO models (model_name)
-VALUES ('us.anthropic.claude-3-7-sonnet-20250219-v1:0');
+VALUES 
+    ('us.anthropic.claude-haiku-4-5-20251001-v1:0'),
+    ('us.anthropic.claude-sonnet-4-5-20250929-v1:0');
--- migrations/6_add_claude_models.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO models (model_name)
-VALUES 
-    ('us.anthropic.claude-haiku-4-5-20251001-v1:0');
-    ('us.anthropic.claude-sonnet-4-5-20250929-v1:0'),
