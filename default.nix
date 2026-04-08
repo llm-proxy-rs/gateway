a2a4ca950e9ad0ace1a3410158a890dc32ee6109
whs <hswongac@gmail.com>
2026-04-08 14:06:18 +0000

    Update 20260408

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {pkgs ? import (import ./npins).nixpkgs {}}: let
   rustPackage = pkgs.rustPlatform.buildRustPackage {
-    cargoHash = "sha256-BX8bvs4I5LLlqtAJ1SReVQ7DcGBwZNrA1c30wLg3UMI=";
+    cargoHash = "sha256-WOEHdYx5jiMoUsgnZuY6S9qip00fCvsoj8OqDQgbGpc=";
     pname = "gateway";
     src = ./.;
     version = "0.1.0";
